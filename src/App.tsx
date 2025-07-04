5d8874bce2859afc4b0f1d762b45d90f3c72985e
Sheddy <okundashedrack@gmail.com>
2025-07-04 20:09:39 +0300

    deleted the pages folder where i was rendering the sections and imported them directly on the entrance file App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import Home from "./pages/Home";
+import type React from "react";
+import { Navigation } from "./layouts/Navigation";
+import { Hero } from "./components/sections/Hero";
+import { About } from "./components/sections/About";
+import { Services } from "./components/sections/Services";
+import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
+import { Footer } from "./components/sections/Footer";
 
-const App = () => {
+const App: React.FC = () => {
 	return (
-		<>
-			<Home />
-		</>
+		<div className="min-h-screen bg-slate-950 text-white">
+			{/* Navigation */}
+			<Navigation />
+
+			{/* Hero Section */}
+			<Hero />
+
+			{/* About Section */}
+			<About />
+
+			{/* Services Section */}
+			<Services />
+
+			{/* Projects Section */}
+			<Projects />
+
+			{/* Contact Section */}
+			<Contact />
+
+			{/* Footer */}
+			<Footer />
+		</div>
 	);
 };
 
