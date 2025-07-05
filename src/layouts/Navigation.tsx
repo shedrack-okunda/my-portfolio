084d532b0720d96570ffb97a087966e661b57966
Sheddy <okundashedrack@gmail.com>
2025-07-05 11:19:30 +0300

    reduce the py of the navbar and inserted the code icon

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Code } from "lucide-react";
 import React from "react";
 
 export const Navigation = () => {
@@ -16,11 +17,13 @@ export const Navigation = () => {
 	return (
 		<>
 			<nav className="fixed top-0 left-0 right-0 z-50 bg-slate-950/80 backdrop-blur-lg border-b border-slate-800">
-				<div className="container mx-auto px-6 py-3">
+				<div className="container mx-auto px-5 py-2">
 					<div className="flex items-center justify-between">
-						<div className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
+						<div className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent hover: cursor-pointer">
 							Sheddy
 						</div>
+
+						<Code className="w-8 h-8 text-blue-400 hover: cursor-pointer" />
 
 						{/* Desktop Menu */}
 						<div className="hidden md:flex items-center space-x-8">
@@ -34,7 +37,7 @@ export const Navigation = () => {
 								<button
 									key={item}
 									onClick={() => scrollToSection(item)}
-									className={`capitalize hover:text-blue-400 transition-colors duration-200 ${
+									className={`capitalize hover:text-blue-400 hover: cursor-pointer transition-colors duration-200 ${
 										currentSection === item
 											? "text-blue-400"
 											: "hover:text-blue-400"
@@ -47,7 +50,7 @@ export const Navigation = () => {
 						{/* Mobile Menu Button */}
 						<button
 							onClick={() => setIsMenuOpen(!isMenuOpen)}
-							className="md:hidden p-2">
+							className="md:hidden hover: cursor-pointer p-2">
 							<div className="w-6 h-6 flex flex-col justify-center space-y-1">
 								<div
 									className={`w-full h-0.5 bg-white transition-all duration-300 ${
@@ -71,7 +74,7 @@ export const Navigation = () => {
 
 					{/* Mobile Menu */}
 					{isMenuOpen && (
-						<div className="md:hidden mt-4 py-4 border-t border-slate-800">
+						<div className="md:hidden mt-4 py-4 border-t border-slate-800 ">
 							{[
 								"home",
 								"about",
@@ -82,7 +85,7 @@ export const Navigation = () => {
 								<button
 									key={item}
 									onClick={() => scrollToSection(item)}
-									className={`block w-full text-left py-2 capitalize hover:text-blue-400 transition-colors duration-200 ${
+									className={`block w-full text-left py-2 capitalize hover:text-blue-400 hover: cursor-pointer transition-colors duration-200 ${
 										currentSection === item
 											? "text-blue-400"
 											: "hover:text-blue-400"
